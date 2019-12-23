2605ba09f8ee8f7ec61a6cc359491dba652aaa1c
Fued Gabriel <fuedgabriel@hotmail.com>
2019-12-23 23:39:01 +0000

    Add files via upload

--- lib/screens/player/player.dart
+++ lib/screens/player/player.dart
@@ -13,7 +13,6 @@ class Pro extends StatefulWidget {
 
   Pro({this.movie, this.videos});
 
-
   @override
   _ProSrcState createState() => _ProSrcState();
 }
@@ -26,18 +25,20 @@ class _ProSrcState extends State<Pro> {
 
   String get src => source[index];
 
-
-
   VideoController vc;
   bool loading = true;
-
-
-
+  var episode;
 
   @override
   void initState() {
     super.initState();
-    vc = VideoController(source: VideoPlayerController.network(src));
+    episode = 0;
+    vc = VideoController(
+      source: VideoPlayerController.network(widget.videos[episode].url),
+      autoplay: true,
+      controllerWidgets: true
+    );
+
   }
 
   @override
@@ -46,83 +47,218 @@ class _ProSrcState extends State<Pro> {
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
 
     return Scaffold(
       body: ListView(
         children: <Widget>[
-          AspectRatio(
-            aspectRatio: 16 / 9,
-            child: VideoBox(
-              controller: vc,
-              children: <Widget>[
-
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: <Widget>[
-                      IconButton(
-                        iconSize: 24,
+          Stack(
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 16 / 9,
+                child: VideoBox(
+                  controller: vc,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: <Widget>[
+                          IconButton(
+                            iconSize: 24,
+                            disabledColor: Colors.white60,
+                            icon: Icon(Icons.arrow_back),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          Text(
+                            'Ep: ' +
+                                (episode + 1).toString() +
+                                ' de ' +
+                                widget.movie.episodes.toString() +
+                                '  ' +
+                                widget.movie.title,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+//                    Align(
+//                      alignment: Alignment(0.9, -0.9),
+//                      child: IconButton(
+//                        iconSize: VideoBox.centerIconSize,
+//                        disabledColor: Colors.white60,
+//                        icon: Icon(Icons.swap_horiz, size: 32,),
+//                        onPressed: () {
+//                        },
+//                      ),
+//                    ),
+                    Align(
+                      alignment: Alignment(0.9, 0),
+                      child: IconButton(
+                        iconSize: VideoBox.centerIconSize,
                         disabledColor: Colors.white60,
-                        icon: Icon(Icons.arrow_back),
+                        icon: Icon(Icons.skip_next),
                         onPressed: () {
-                          Navigator.of(context).pop();
+                          print(episode);
+                          episode=episode+1;
+                          print(widget.videos[episode].url);
+                          print(episode);
+//                          vc.dispose();
+                          vc.setAutoplay(true);
+                          vc.showVideoCtrl(true);
+                          vc.autoplay;
+                          print(widget.videos[episode].url);
+                          vc.setSource(VideoPlayerController.network(widget.videos[episode].url));
                         },
                       ),
-                      Text('Ep: '+(index+1).toString()+' de '+widget.movie.episodes.toString()+'  '+widget.movie.title,
-                        style: TextStyle(
-                            color: Colors.white
-                        ),
+                    ),
+                    Align(
+                      alignment: Alignment(-0.9, 0),
+                      child: IconButton(
+                        iconSize: VideoBox.centerIconSize,
+                        disabledColor: Colors.white60,
+                        icon: Icon(Icons.skip_previous),
+                        onPressed: () {
+                          episode=episode-1;
+                          vc.setAutoplay(true);
+                          vc.showVideoCtrl(true);
+                          vc.setSource(VideoPlayerController.network(widget.videos[episode].url));
+                        },
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                Align(
-                  alignment: Alignment(0.9, 0),
-                  child: IconButton(
-                    iconSize: VideoBox.centerIconSize,
-                    disabledColor: Colors.white60,
-                    icon: Icon(Icons.skip_next),
-                    onPressed: () {
-                      var newindex = index + 1;
-                      if (newindex >= source.length) {
-                        newindex = 0;
-                      }
-                      setState(() {
-                        index = newindex;
-                      });
-                      vc.setSource(VideoPlayerController.network(src));
-                    },
-                  ),
-                ),
-                Align(
-                  alignment: Alignment(-0.9, 0),
-                  child: IconButton(
-                    iconSize: VideoBox.centerIconSize,
-                    disabledColor: Colors.white60,
-                    icon: Icon(Icons.skip_previous),
-                    onPressed: () {
-                      var newindex = index - 1;
-                      if (newindex < 0) {
-                        newindex = source.length - 1;
-                      }
-                      setState(() {
-                        index = newindex;
-                      });
-                      vc.setAutoplay(true);
-                      vc.showVideoCtrl(true);
-                      vc.setSource(VideoPlayerController.network(src));
-                    },
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
           Stack(
             children: <Widget>[
-              ContentScroll(images: widget.videos, movies: widget.movie,),
+              Container(
+                height: (height - 170),
+                child: ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemCount: widget.videos.length,
+                    itemBuilder: (BuildContext, int conta) {
+                      return Stack(
+                        children: <Widget>[
+                          Container(
+                            height: 90,
+                            child: Card(
+                              elevation: 5,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Padding(
+                                      padding: EdgeInsets.only(left: 6),
+                                      child: GestureDetector(
+                                        child: Stack(
+                                          children: <Widget>[
+                                            Image(
+                                              image: NetworkImage(
+                                                  'https://3.bp.blogspot.com/-2bwBfYvt0Rk/WDM-lfH574I/AAAAAAAABos/Jb4MauEsi8072H-4IOy5b4y9Joi_ia5_ACLcB/s1600/Untitled.png'),
+                                              fit: BoxFit.contain,
+                                              height: 128,
+                                              width: 128,
+                                            ),
+                                            Padding(
+                                              padding: EdgeInsets.only(
+                                                  top: 10, left: 5),
+                                              child: Icon(
+                                                Icons.play_circle_outline,
+                                                size: 32,
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        onTap: () {
+                                          episode = conta;
+                                          vc.setAutoplay(true);
+                                          vc.showVideoCtrl(true);
+                                          vc.setSource(VideoPlayerController.network(widget.videos[conta].url));
+                                        },
+                                      )),
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 10, left: 5),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Opacity(
+                                            opacity: 0.8,
+                                            child: Padding(
+                                              padding: EdgeInsets.only(top: 5),
+                                              child: Text(
+                                                'Episódio ' +
+                                                    widget.videos[conta].episode.toString() +
+                                                    '   ',
+                                                style: TextStyle(
+                                                    fontSize: 16,
+                                                    fontWeight: FontWeight.bold,
+                                                    fontStyle:
+                                                        FontStyle.italic),
+                                              ),
+                                            )),
+                                        Opacity(
+                                            opacity: 0.6,
+                                            child: Padding(
+                                              padding: EdgeInsets.only(top: 5),
+                                              child: Text(
+                                                widget.movie.air
+                                                    .toString()
+                                                    .replaceAll(
+                                                        'T00:00:00.000Z', ''),
+                                                style: TextStyle(
+                                                    fontSize: 13,
+                                                    fontWeight: FontWeight.bold,
+                                                    fontStyle:
+                                                        FontStyle.italic),
+                                              ),
+                                            )),
+                                      ],
+                                    ),
+                                  ),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    mainAxisSize: MainAxisSize.max,
+                                    children: <Widget>[
+                                      Container(
+                                        height: 20,
+                                        child: IconButton(
+                                          icon: Icon(Icons.file_download),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding:
+                                            EdgeInsets.only(top: 20, left: 12),
+                                        child: Container(
+                                          height: 20,
+                                          child: IconButton(
+                                            icon: Icon(Icons.screen_share),
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      );
+                    }),
+              ),
+//              ContentScroll(
+//                images: widget.videos,
+//                movies: widget.movie,
+//              ),
             ],
           ),
         ],
--- lib/screens/player/widget/tile.dart
+++ lib/screens/player/widget/tile.dart
@@ -1,8 +1,9 @@
-
 //widget
 
 import 'package:animese/request/Videos.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 //request
 import 'package:animese/request/Animes.dart';
 
@@ -11,107 +12,119 @@ class ContentScroll extends StatelessWidget {
   final List<ListVideo> images;
   final ListAnime movies;
 
-  ContentScroll({
-    this.images, this.movies
-  });
-
-
+  ContentScroll({this.images, this.movies});
 
   Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: int.parse((images[images.length-1].season)),
-      itemBuilder: (BuildContext context, int index) {
-        return ExpansionTile(
-          leading: Icon(Icons.movie),
-          title: Text('Temporada '+movies.seasons.toString()),
-          children: <Widget>[
-            ListView.builder(
-                shrinkWrap: true,
-                itemCount: images.length,
-                itemBuilder: (BuildContext, int conta){
-                  return Container(
-                    height: 90,
-                    child: Card(
-                      elevation: 5,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Padding(
-                            padding: EdgeInsets.only(left: 6),
-                            child: Image(
-                              image: NetworkImage('https://3.bp.blogspot.com/-2bwBfYvt0Rk/WDM-lfH574I/AAAAAAAABos/Jb4MauEsi8072H-4IOy5b4y9Joi_ia5_ACLcB/s1600/Untitled.png'),
-                              fit: BoxFit.contain,
-                              height: 128,
-                              width: 128,
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(
-                                top: 10, left: 5
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
+    var height = MediaQuery.of(context).size.height;
+    return Container(
+      height: (height-170),
+      child: ListView.builder(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemCount: 5,
+          itemBuilder: (BuildContext, int conta) {
+            return Stack(
+              children: <Widget>[
+                Container(
+                  height: 90,
+                  child: Card(
+                    elevation: 5,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(left: 6),
+                          child: GestureDetector(
+                            child: Stack(
                               children: <Widget>[
-                                Opacity(
-                                    opacity: 0.8,
-                                    child: Padding(
-                                      padding: EdgeInsets.only(top: 5) ,
-                                      child: Text(
-                                        'Episódio ' + images[0].episode.toString() +'   ',
-                                        style: TextStyle(
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.bold,
-                                            fontStyle: FontStyle.italic
-                                        ),
-                                      ),)
+                                Image(
+                                  image: NetworkImage(
+                                      'https://3.bp.blogspot.com/-2bwBfYvt0Rk/WDM-lfH574I/AAAAAAAABos/Jb4MauEsi8072H-4IOy5b4y9Joi_ia5_ACLcB/s1600/Untitled.png'),
+                                  fit: BoxFit.contain,
+                                  height: 128,
+                                  width: 128,
                                 ),
-                                Opacity(
-                                    opacity: 0.6,
-                                    child: Padding(
-                                      padding: EdgeInsets.only(top: 5) ,
-                                      child: Text(
-                                        movies.air.toString().replaceAll('T00:00:00.000Z', ''),
-                                        style: TextStyle(
-                                            fontSize: 13,
-                                            fontWeight: FontWeight.bold,
-                                            fontStyle: FontStyle.italic
-                                        ),
-                                      ),)
-                                ),
-                              ],),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            mainAxisSize: MainAxisSize.max,
-                            children: <Widget>[
-                              Container(
-                                height: 20,
-                                child: IconButton(
-                                  icon: Icon(Icons.file_download),
-                                ),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.only(top: 20, left: 12),
-                                child: Container(
-                                  height: 20,
-                                  child: IconButton(
-                                    icon: Icon(Icons.screen_share),
+                                Padding(
+                                  padding: EdgeInsets.only(top: 10, left: 5),
+                                  child: Icon(
+                                    Icons.play_circle_outline,
+                                    size: 32,
+                                    color: Colors.black,
                                   ),
                                 ),
-                              )
+                              ],
+                            ),
+                            onTap: (){
+
+                            },
+                          )
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 10, left: 5),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Opacity(
+                                  opacity: 0.8,
+                                  child: Padding(
+                                    padding: EdgeInsets.only(top: 5),
+                                    child: Text(
+                                      'Episódio ' +
+                                          images[0].episode.toString() +
+                                          '   ',
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                          fontStyle: FontStyle.italic),
+                                    ),
+                                  )),
+                              Opacity(
+                                  opacity: 0.6,
+                                  child: Padding(
+                                    padding: EdgeInsets.only(top: 5),
+                                    child: Text(
+                                      movies.air
+                                          .toString()
+                                          .replaceAll('T00:00:00.000Z', ''),
+                                      style: TextStyle(
+                                          fontSize: 13,
+                                          fontWeight: FontWeight.bold,
+                                          fontStyle: FontStyle.italic),
+                                    ),
+                                  )),
                             ],
                           ),
-                        ],
-                      ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          mainAxisSize: MainAxisSize.max,
+                          children: <Widget>[
+                            Container(
+                              height: 20,
+                              child: IconButton(
+                                icon: Icon(Icons.file_download),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(top: 20, left: 12),
+                              child: Container(
+                                height: 20,
+                                child: IconButton(
+                                  icon: Icon(Icons.screen_share),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
                     ),
-                  );
-                })
-          ],
-        );
-        },
+                  ),
+                )
+              ],
+            );
+          }),
     );
   }
 }

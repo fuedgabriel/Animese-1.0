e5e67ed2f528bc2e3ed48b727b5afde1d5d3e3a1
Fued Gabriel <fuedgabriel@hotmail.com>
2020-03-26 21:53:13 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+
+
+import 'screens/home_page/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'screens/config/theme/bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'screens/config/config.dart';
+import 'screens/login/LoginPage.dart';
+import 'screens/animes_list/animes_screen.dart';
+import 'screens/suporte/suporte.dart';
+import 'screens/favoritos/favoritos.dart';
+import 'screens/premium/Premium.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      builder: (context) => ThemeBloc(),
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+        builder: _buildWithTheme,
+      ),
+    );
+  }
+
+  Widget _buildWithTheme(BuildContext context, ThemeState state) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      theme: state.themeData,
+      routes: <String, WidgetBuilder>{
+        '/Config': (BuildContext context) => Config(),
+        '/Home': (BuildContext context) => HomeScreen(),
+        '/Suporte': (BuildContext context) => Suporte(),
+        '/Login': (BuildContext context) => LoginPage(),
+        '/Animes': (BuildContext context) => AnimesScreen(),
+        '/Favoritos': (BuildContext context) => Favoritos(),
+        '/Premium': (BuildContext context) => PremiumPage(),
+      },
+      home: HomeScreen(),
+    );
+  }
+}
